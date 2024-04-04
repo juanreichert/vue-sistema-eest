c3a105d1ef6edfa791f3ef097deaca8f5c46de38
Juan Reichert <juanreichert@yahoo.com>
2024-04-04 20:56:21 -0300

    nombre de creador

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -17,7 +17,7 @@
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          La aplicacion esta construida con el framework Vue.js + Versel
+          La aplicacion esta construida con el framework Vue.js + Versel y la hizo juan reichert
         </h1>
 
         <p class="subheading font-weight-regular">
